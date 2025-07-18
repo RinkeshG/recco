edf829fb9d2fa87b98a6ecf06b93b6a9a2d2d2b8
Rinkesh Gorasia <reachrinkesh@gmail.com>
2025-07-18 13:33:34 +0530

    Update page.tsx

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -1,10 +1,1 @@
-// app/[username]/page.tsx
-import React from 'react';
-
-export default function UserPage({
-  params,
-}: {
-  params: { username: string };
-}) {
-  return <div>Profile page for {params.username}</div>;
-}
+export {};   // marks the file as a module
